cfa33e0b11fce703d3d214404ea0ad3e432c806c
vivek <vivekkumarsingh302@gmail.com>
2024-09-05 16:10:22 +0530

    First commmit in develop branch

--- Alerts.py
+++ Alerts.py
@@ -18,5 +18,18 @@ Alert.accept()
 Alert.dismiss()
 
 
+driver = webdriver.Edge()
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+name = "Vivek"
+driver.find_element(By.ID, "name").send_keys(name)
+driver.find_element(By.ID, "alertbtn").click()
+Alert = driver.switch_to.alert
+alertText = Alert.text
+assert name in alertText
+print(alertText)
+Alert.accept()
+Alert.dismiss()
+
+
 
 time.sleep(3)
